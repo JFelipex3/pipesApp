18fc874ebac33b04d629e518a7b269617a543ac7
Jhonnatan Felipe <jfelipe.machuca@gmail.com>
2021-10-28 20:11:43 -0300

    Pipes Personalizados

--- src/app/ventas/pages/ordenar/ordenar.component.ts
+++ src/app/ventas/pages/ordenar/ordenar.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Color, Heroe } from '../../interfaces/ventas.interface';
 
 @Component({
   selector: 'app-ordenar',
@@ -6,11 +7,48 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class OrdenarComponent implements OnInit {
+export class OrdenarComponent {
 
-  constructor() { }
+  enMayusculas: boolean = true;
+  ordernarPor: string = '';
+  heroes: Heroe[] = [
+    {
+      nombre: 'Superman',
+      vuela: true,
+      color: Color.azul
+    },
+    {
+      nombre: 'Batman',
+      vuela: false,
+      color: Color.negro
+    },
+    {
+      nombre: 'Robin',
+      vuela: false,
+      color: Color.rojo
+    },
+    {
+      nombre: 'Flash',
+      vuela: false,
+      color: Color.rojo
+    },
+    {
+      nombre: 'Linterna Verde',
+      vuela: true,
+      color: Color.verde
+    },
+    {
+      nombre: 'Darevil',
+      vuela: false,
+      color: Color.rojo
+    }
+  ];
 
-  ngOnInit(): void {
+  cambiarEnMayusculas(){
+    this.enMayusculas = !this.enMayusculas;
   }
 
+  cambiarOrden( valor: string){
+    this.ordernarPor = valor;
+  }
 }
